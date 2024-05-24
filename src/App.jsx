e19a9010c9f1ceee00a6fae3b15114c1629079e6
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-24 21:46:47 +0530

    install the tsparticle for dynamic bg , also add component to app and mow the header and navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Header from "./components/header/Header";
+import MainBg from "./components/MainBg";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <MainBg></MainBg>
     </>
   );
 }
---
+++ src/components/MainBg.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+
+function MainBg() {
+  const particlesInit = useCallback(async (engine) => {
+    await loadSlim(engine);
+  }, []);
+  const particlesLoaded = useCallback(async (container) => {}, []);
+  return (
+    <div className="fixed -z-10">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#ffffff",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: false,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#e374d0",
+            },
+            links: {
+              color: "#627ec4",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "triangle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div>
+  );
+}
+
+export default MainBg;
