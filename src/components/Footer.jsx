f4d1f6f9cdd25b1d567d83992a5eb474f6d52b9d
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-28 21:43:52 +0530

    Add path of links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,25 +9,35 @@ function Footer() {
         <div className="text-yellow-500">
           <span className="">© 2024 DEEPAK PANCHAL</span>
         </div>
-        <div  >
+        <div>
           <ul className="flex flex-wrap text-xl md:text-2xl py-4 gap-4 md:gap-7">
             <li>
-              <a href="">
+              <a
+                href="https://www.instagram.com/_deepak__.__panchal"
+                target="_blank"
+              >
                 <FaInstagram />
               </a>
             </li>
             <li>
-              <a href="">
+              <a href="https://github.com/tech-deepakk" target="_blank">
                 <GrGithub />
               </a>
             </li>
 
             <li>
-              <a href="">
-                <GrLinkedinOption/>
+              <a
+                href="https://www.linkedin.com/in/deepak-panchal-733806290/"
+                target="_blank"
+              >
+                <GrLinkedinOption />
               </a>
             </li>
-            <li><a href=""><GrMail/></a></li>
+            <li>
+              <a href="mailto:tech.deepakpanchal@gmail.com" target="_blank">
+                <GrMail />
+              </a>
+            </li>
           </ul>
         </div>
       </footer>
